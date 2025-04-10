69e8b1797ea5b051601784cb2fa0f073e947287b
naveengopi3 <naveengopi135@gmail.com>
2025-04-10 19:55:51 +0530

    modified files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:taskify/bloc/auth_bloc/auth_bloc.dart';
 import 'package:taskify/firebase_options.dart';
 import 'package:taskify/screens/splash_screen/splash_screen.dart';
 import 'package:taskify/utils/colors.dart';
@@ -16,20 +14,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AuthBloc()),
-        ],
-       child: MaterialApp(
-        title: 'Taskify',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: CustomColors.korangeMedium,
-          ),
-        ),
-        home: SplashScreen(),
-      ),
-    );
+    return MaterialApp(
+     title: 'Taskify',
+     debugShowCheckedModeBanner: false,
+     theme: ThemeData(
+       colorScheme: ColorScheme.fromSwatch().copyWith(
+         primary: CustomColors.korangeMedium,
+       ),
+     ),
+     home: SplashScreen(),
+          );
   }
 }
